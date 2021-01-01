acfed07a6327acbbb00519360ad2391cc5521143
rrd <rrd@1108.cc>
2021-01-01 10:40:26 +0100

    runtime cache added

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,37 @@ module.exports = {
   pwa: {
     workboxPluginMode: "GenerateSW",
     workboxOptions: {
-      exclude: ["robots.txt"]
+      exclude: ["robots.txt", /\.map$/],
+      runtimeCaching: [
+        {
+          urlPattern: process.env.VUE_APP_API_URL,
+          handler: "NetworkFirst",
+          options: {
+            cacheName: "api"
+          }
+        },
+        {
+          urlPattern: /^https:\/\/fonts\.googleapis\.com\//,
+          handler: "StaleWhileRevalidate",
+          options: {
+            cacheName: "google-fonts-stylesheets"
+          }
+        },
+        {
+          urlPattern: /^https:\/\/fonts\.gstatic\.com\//,
+          handler: "CacheFirst",
+          options: {
+            cacheName: "google-fonts-webfonts",
+            cacheableResponse: {
+              statuses: [0, 200]
+            },
+            expiration: {
+              maxAgeSeconds: 60 * 60 * 24 * 365,
+              maxEntries: 5
+            }
+          }
+        }
+      ]
     }
   }
 };
