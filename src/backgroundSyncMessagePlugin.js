6dfe3f8e9a0d5f34d9c1f3ee9789f70e5327a705
rrd <rrd@1108.cc>
2021-01-07 21:43:04 +0100

    response message from sw received in app

--- src/backgroundSyncMessagePlugin.js
+++ src/backgroundSyncMessagePlugin.js
@@ -18,34 +18,43 @@ const replayRequestsMessage = async function() {
       // the original function call does not do anything with the response
       fetch(entry.request.clone())
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => {
+          self.clients.matchAll()
+            .then(function (clients) {
+              if (clients && clients.length) {
+                //Respond to last focused tab
+                clients[0].postMessage(data)
+              }
+            })
+          })
+      // custom modification of the method ends here
 
       if (process.env.NODE_ENV !== 'production') {
         logger.log(`Request for '${getFriendlyURL(entry.request.url)}'` +
-            `has been replayed in queue '${this._name}'`);
+            `has been replayed in queue '${this._name}'`)
       }
     } catch (error) {
       await this.unshiftRequest(entry);
 
       if (process.env.NODE_ENV !== 'production') {
         logger.log(`Request for '${getFriendlyURL(entry.request.url)}'` +
-            `failed to replay, putting it back in queue '${this._name}'`);
+            `failed to replay, putting it back in queue '${this._name}'`)
       }
-      throw new WorkboxError('queue-replay-failed', {name: this._name});
+      throw new WorkboxError('queue-replay-failed', {name: this._name})
     }
   }
   if (process.env.NODE_ENV !== 'production') {
     logger.log(`All requests in queue '${this.name}' have successfully ` +
-        `replayed; the queue is now empty!`);
+        `replayed; the queue is now empty!`)
   }
 }
 
 class BackgroundSyncMessagePlugin extends workbox.backgroundSync.Plugin {
 
   constructor(...queueArgs) {
-    // we should add a custom onSync function to modify replayRequests()
+    // we should add a custom onSync function to modify replayRequests() as fetchDidSucceed is not called from this version of the bgsync plugin
     let args = { ...queueArgs}
-    console.info('...queueArgs', args[0], { ...args[1], onSync: replayRequestsMessage })
+    //console.info('...queueArgs', args[0], { ...args[1], onSync: replayRequestsMessage })
     super(args[0], { ...args[1], onSync: replayRequestsMessage })
     //super(...queueArgs)
     console.warn('BackgroundSyncMessagePlugin', this)
