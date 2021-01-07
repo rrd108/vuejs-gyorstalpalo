ed3604b8fbcae3c225e7d53780d2667c215f2fc5
rrd <rrd@1108.cc>
2021-01-07 19:50:57 +0100

    fetchDidFail caled from the plugin and backgroundsync is working

--- src/backgroundSyncMessagePlugin.js
+++ src/backgroundSyncMessagePlugin.js
@@ -13,19 +13,22 @@ workbox.routing.registerRoute(
 class BackgroundSyncMessagePlugin extends workbox.backgroundSync.Plugin {
 
   constructor(...queueArgs) {
-    super(queueArgs)
-    console.warn('BackgroundSyncMessagePlugin constructor called. queue args:', queueArgs)
+    console.info('...queueArgs', ...queueArgs)
+    super(...queueArgs)
+    console.warn('BackgroundSyncMessagePlugin', this)
+    this.fetchDidSucceed = this.fetchDidSucceed.bind(this)
   }
 
-  fetchDidFail(request) {
-    this._queue.pushRequest(request)
+  fetchDidFail({ request }) {
+    super.fetchDidFail({ request });
+    console.warn('fetchDidFail', { request })
   }
 
   fetchDidSucceed({request, response, event, state}) {
     console.warn('fetchDidSucceed', {request, response, event, state})
     return response
   }
-  handlerWillRespond({request, response, event, state}) {
+  /*handlerWillRespond({request, response, event, state}) {
     console.warn('handlerWillRespond', {request, response, event, state})
     return response
   }
@@ -34,7 +37,7 @@ class BackgroundSyncMessagePlugin extends workbox.backgroundSync.Plugin {
   }
   handlerDidComplete({request, response, error, event, state}) {
     console.warn('handlerDidComplete', {request, response, error, event, state})
-  }
+  }*/
 }
 
 //export {BackgroundSyncMessagePlugin}
